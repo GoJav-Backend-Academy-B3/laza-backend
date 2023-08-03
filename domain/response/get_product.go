dd860b4bf740fefde61d5b18d30b954e4eae8ef1
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-03 14:09:05 +0700

    add product response

---
+++ domain/response/get_product.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"time"
+
+	"github.com/phincon-backend/laza/domain/entities"
+)
+
+type Product struct {
+	Id          uint64    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ImageUrl    string    `json:"image_url"`
+	Price       float32   `json:"price"`
+	CategoryId  uint64    `json:"category_id"`
+	BrandId     uint64    `json:"brand_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func (p *Product) FillFromEntity(e entities.Product) {
+	p.Id = e.Id
+	p.Name = e.Name
+	p.Description = e.Description
+	p.ImageUrl = e.ImageUrl
+	p.Price = e.Price
+	p.CategoryId = e.CategoryId
+	p.BrandId = e.BrandId
+	p.CreatedAt = e.CreatedAt
+	p.UpdatedAt = e.UpdatedAt
+}
