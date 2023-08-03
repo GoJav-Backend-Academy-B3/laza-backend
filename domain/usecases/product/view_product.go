acb59a08d0259f4147cb4e2549952201b37b6e3d
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-03 14:07:09 +0700

    change uint32 type to uint64

--- domain/usecases/product/view_product.go
+++ domain/usecases/product/view_product.go
@@ -3,5 +3,5 @@ package product
 import "github.com/phincon-backend/laza/domain/entities"
 
 type ViewProductUsecase interface {
-	Execute(offset, limit uint32) ([]entities.Product, error)
+	Execute(offset, limit uint64) ([]entities.Product, error)
 }
