e55d6d770a16827f2b11e64eb9f7592c93052029
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-03 16:26:17 +0700

    instantiate gorm db to be used by repo

--- cmd/server/provider/products_handler.go
+++ cmd/server/provider/products_handler.go
@@ -7,13 +7,17 @@ import (
 	u "github.com/phincon-backend/laza/internal/usecase/product"
 
 	r "github.com/phincon-backend/laza/internal/repo/product"
+
+	b "github.com/phincon-backend/laza/internal/db"
 )
 
 func NewProductsHandler() d.HandlerInterface {
 
 	// TODO: instantiate or get db
+	db := b.GetPostgreSQLConnection()
+	gorm := db.(*b.PsqlDB).Dbs
 
-	repo := r.NewProductRepo(db)
+	repo := r.NewProductRepo(gorm)
 
 	viewProduct := u.NewViewProductUsecaseImpl(repo)
 	return h.NewProductHandler("/products", viewProduct)
