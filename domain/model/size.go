88a93f83731e730e1d656c8da536de20ac12eac8
ariwiraa <ariwiraatmaja@gmail.com>
2023-08-03 16:58:21 +0700

    delete file and import package

--- domain/model/size.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Size struct {
-	Id   int    `json:"id,omitempty"`
-	Size string `json:"size,omitempty"`
-}
--- domain/model/size_product.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type SizeProduct struct {
-	ProductId int `json:"product-id,omitempty"`
-	SizeId    int `json:"size-id,omitempty"`
-}
