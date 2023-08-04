b3467b5b6ba2ad95e71e3b5534ef4bb6a6ad5b1b
unknown <aldi.ramdan96@gmail.com>
2023-08-04 14:15:52 +0700

    Update repo user

--- internal/repo/user/get_by_id.go
+++ internal/repo/user/get_by_id.go
@@ -6,11 +6,11 @@ import (
 	"github.com/phincon-backend/laza/domain/model"
 )
 
-func (r *UserRepo) GetById(id string) (*model.User, error) {
+func (r *UserRepo) GetById(id uint64) (*model.User, error) {
 	var e model.User
 	if err := r.db.Find(&e, "id = ?", id).Error; err != nil {
 		return nil, errors.New("failed to get data")
 	}
-	
+
 	return &e, nil
 }
