f22f8d3008697999749db1f1a0cefde2ad5e1545
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-08-04 14:42:30 +0700

    edit response to helper getreponse

--- internal/handler/wishlist/update.go
+++ internal/handler/wishlist/update.go
@@ -7,30 +7,30 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/phincon-backend/laza/domain/model"
-	"github.com/phincon-backend/laza/domain/response"
+	"github.com/phincon-backend/laza/helper"
 )
 
 func (h *getWishlistHandler) Put(ctx *gin.Context) {
 	ws := model.Wishlist{}
 	userid, err := strconv.ParseUint(ctx.Param("userId"), 10, 32)
 	if err != nil {
-		response := response.GetResponse(err, http.StatusBadRequest, true)
+		response := helper.GetResponse(err, http.StatusBadRequest, true)
 		response.Send(ctx)
 	}
 	productid, err := strconv.ParseUint(ctx.Param("productId"), 10, 32)
 	if err != nil {
-		response := response.GetResponse(err, http.StatusBadRequest, true)
+		response := helper.GetResponse(err, http.StatusBadRequest, true)
 		response.Send(ctx)
 	}
 
 	ws.UserId, ws.ProductId = userid, productid
 	rs, err := h.updateWishlistUsecase.Execute(ws)
 	if err != nil {
-		response := response.GetResponse(err, http.StatusInternalServerError, true)
+		response := helper.GetResponse(err, http.StatusInternalServerError, true)
 		response.Send(ctx)
 	}
 
 	fmt.Println(rs)
-	response := response.GetResponse(rs, http.StatusOK, false)
+	response := helper.GetResponse(rs, http.StatusOK, false)
 	response.Send(ctx)
 }
