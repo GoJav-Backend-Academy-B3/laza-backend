fa60bf3cd30683fe741bb55a63ea55a6f86a1caa
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-07 21:13:25 +0700

    get errors and immediate return if exists

--- internal/repo/product/update.go
+++ internal/repo/product/update.go
@@ -11,6 +11,10 @@ func (r *ProductRepo) Update(id_r any, e model.Product) (product model.Product, 
 	tx := r.db.First(&product, "id = ?", id)
 	// TODO: result.Error might return gorm.ErrRecordNotFound if
 	//       no `id` can be found
+	err = tx.Error
+	if err != nil {
+		return
+	}
 
 	// Modify data
 	product.Update(e)
