3cbf8a6e5c1777d761afdc9ac9b2e7544bcacc82
ariwiraa <ariwiraatmaja@gmail.com>
2023-08-07 14:11:34 +0700

    feat: add swagger for address endpoint

--- internal/repo/address/get_all.go
+++ internal/repo/address/get_all.go
@@ -3,7 +3,7 @@ package address
 import "github.com/phincon-backend/laza/domain/model"
 
 func (r *addressRepo) GetAllByUserId(userId uint64) (addresses []model.Address, err error) {
-	err = r.db.Where("user_id = ?", userId).Find(&addresses).Error
+	err = r.db.Where("user_id = ?", userId).Preload("users").Find(&addresses).Error
 	if err != nil {
 		return
 	}
--- internal/repo/address/get_by_id.go
+++ internal/repo/address/get_by_id.go
@@ -5,7 +5,7 @@ import "github.com/phincon-backend/laza/domain/model"
 func (r *addressRepo) GetById(id any) (model.Address, error) {
 	var address model.Address
 
-	err := r.db.First(&address, "id = ?", id).Error
+	err := r.db.Preload("users").First(&address, "id = ?", id).Error
 	if err != nil {
 		return address, err
 	}
