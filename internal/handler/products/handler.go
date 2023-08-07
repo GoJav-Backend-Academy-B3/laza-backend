b298b4c810fc2fb0569f374e779ad9be454da7c2
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-07 20:31:57 +0700

    use update and delete usecase in handler

--- internal/handler/products/handler.go
+++ internal/handler/products/handler.go
@@ -10,7 +10,9 @@ import (
 type productHandler struct {
 	path                       string
 	createProductUsecase       uc.CreateProductUsecase
+	updateProductUsecase       uc.UpdateProductUsecase
 	viewProductUsecase         uc.ViewProductUsecase
+	deleteProductUsecase       uc.DeleteProductUsecase
 	searchProductByNameUsecase uc.SearchProductByNameUsecase
 }
 
@@ -26,18 +28,32 @@ func (h *productHandler) GetHandlers() (hs []hd.HandlerStruct) {
 		Path:        h.path,
 		HandlerFunc: h.post,
 	})
+	hs = append(hs, hd.HandlerStruct{
+		Method:      http.MethodPut,
+		Path:        h.path + "/:id",
+		HandlerFunc: h.put,
+	})
+	hs = append(hs, hd.HandlerStruct{
+		Method:      http.MethodDelete,
+		Path:        h.path + "/:id",
+		HandlerFunc: h.delete,
+	})
 	return
 }
 
 func NewProductHandler(
 	path string,
 	createProductUsecase uc.CreateProductUsecase,
+	updateProductUsecase uc.UpdateProductUsecase,
 	viewProductUsecase uc.ViewProductUsecase,
+	deleteProductUsecase uc.DeleteProductUsecase,
 	searchProductByNameUsecase uc.SearchProductByNameUsecase) hd.HandlerInterface {
 	return &productHandler{
 		path:                       path,
 		createProductUsecase:       createProductUsecase,
+		updateProductUsecase:       updateProductUsecase,
 		viewProductUsecase:         viewProductUsecase,
+		deleteProductUsecase:       deleteProductUsecase,
 		searchProductByNameUsecase: searchProductByNameUsecase,
 	}
 }
