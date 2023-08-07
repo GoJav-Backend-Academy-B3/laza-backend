b2e88b2e7d5b0f603f9471792a6da008bf70580a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-07 15:31:21 +0700

    create UploadImageFile function to accept multipartFile

--- helper/cloudinary.go
+++ helper/cloudinary.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
@@ -71,6 +73,42 @@ func UploadImage(c *gin.Context) (url string, err error) {
 	if err != nil {
 		return
 	}
+
+	return result.SecureURL, nil
+}
+
+func UploadImageFile(file multipart.File) (url string, err error) {
+
+	buff := make([]byte, 512)
+	_, err = file.Read(buff)
+	if err != nil {
+		return
+	}
+
+	filetype := http.DetectContentType(buff)
+	if filetype != "image/jpeg" && filetype != "image/png" && filetype != "image/jpg" && filetype != "image/webp" {
+		err = errors.New("file format is not allowed. Please upload a JPEG, JPG, PNG or WEBP image")
+		return
+	}
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return
+	}
+
+	imageId := strconv.Itoa(int(time.Now().UnixNano()))
+	creds := GetCloudinaryCredentials()
+	cld, err := cloudinary.NewFromParams(creds.Name, creds.APIKey, creds.Secret)
+	if err != nil {
+		return
+	}
+
+	ctx := context.Background()
+	result, err := cld.Upload.Upload(ctx, file, uploader.UploadParams{PublicID: imageId})
+	if err != nil {
+		return
+	}
+	log.Println(result.Error)
 
 	return result.SecureURL, nil
 }
