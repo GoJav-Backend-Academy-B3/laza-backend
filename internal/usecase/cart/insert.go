930e5526714af94b7b55d63f160a1785066f0df5
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-08-08 10:55:41 +0700

    edit provider & usecese cart

--- internal/usecase/cart/insert.go
+++ internal/usecase/cart/insert.go
@@ -1,26 +1,41 @@
 package cart
 
 import (
+	"errors"
 	"net/http"
 
 	m "github.com/phincon-backend/laza/domain/model"
 	d "github.com/phincon-backend/laza/domain/repositories"
+	action "github.com/phincon-backend/laza/domain/repositories/product"
 	usecase "github.com/phincon-backend/laza/domain/usecases/cart"
+	"github.com/phincon-backend/laza/helper"
+	"gorm.io/gorm"
+
 	h "github.com/phincon-backend/laza/helper"
 )
 
 type insertCartUsecase struct {
-	insertCartRepo d.InsertAction[m.Cart]
+	insertCartRepo       d.InsertAction[m.Cart]
+	getProductByIdAction action.GetProductByIdAction[m.Product]
 }
 
 func (uc *insertCartUsecase) Execute(userId uint64, productId uint64) *h.Response {
+
+	_, err := uc.getProductByIdAction.GetProductById(productId)
+
+	if err == gorm.ErrRecordNotFound {
+		return helper.GetResponse(errors.New("product not found").Error(), http.StatusNotFound, true)
+	}
+
+	if err != nil {
+		return helper.GetResponse(err.Error(), http.StatusInternalServerError, true)
+	}
+
 	cart := m.Cart{
 		UserId:    userId,
 		ProductId: productId,
 		Quantity:  1,
 	}
-
-	// check the product first
 	rs, err := uc.insertCartRepo.Insert(cart)
 	if err != nil {
 		return h.GetResponse(err.Error(), http.StatusInternalServerError, true)
@@ -29,8 +44,12 @@ func (uc *insertCartUsecase) Execute(userId uint64, productId uint64) *h.Respons
 	return h.GetResponse(rs, http.StatusOK, false)
 }
 
-func NewinsertCartUsecase(icp d.InsertAction[m.Cart]) usecase.InsertCartUsecase {
+func NewinsertCartUsecase(
+	icp d.InsertAction[m.Cart],
+	gpi action.GetProductByIdAction[m.Product],
+) usecase.InsertCartUsecase {
 	return &insertCartUsecase{
-		insertCartRepo: icp,
+		insertCartRepo:       icp,
+		getProductByIdAction: gpi,
 	}
 }
