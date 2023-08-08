a782746f4dbad0f5e5b233b1b77abe3ced76b5a4
Adimas Putra Pratama <adimaspph@AdimasMacUltra.local>
2023-08-08 09:41:31 +0700

    feat: add get product order by id repo

--- internal/repo/product_order/retrieve_product_order_by_id.go
+++ internal/repo/product_order/retrieve_product_order_by_id.go
@@ -1,9 +1,11 @@
 package product_order
 
-//import "github.com/phincon-backend/laza/domain/model"
-//
-//func (r *ProductOrderRepo) GetById(id string) (po model.ProductOrder, err error) {
-//	tx := r.db.First(&po, id)
-//	err = tx.Error
-//	return
-//}
+import "github.com/phincon-backend/laza/domain/model"
+
+func (r *ProductOrderRepo) GetById(orderId any) (po model.ProductOrder, err error) {
+	orderIdConv := orderId.(string)
+
+	tx := r.db.First(&po, orderIdConv)
+	err = tx.Error
+	return
+}
