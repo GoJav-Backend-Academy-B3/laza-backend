e701d4660b8f9ec46416169a35f44d7081dfdd5f
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-09 20:08:56 +0700

    fix product repo

--- internal/repo/product/get_with_limit.go
+++ internal/repo/product/get_with_limit.go
@@ -3,7 +3,10 @@ package product
 import "github.com/phincon-backend/laza/domain/model"
 
 func (r *ProductRepo) GetWithLimit(limit, offset uint64) (es []model.Product, err error) {
-	tx := r.db.Find(&es).Offset(int(offset)).Limit(int(limit))
+	tx := r.db.
+		Limit(int(limit)).
+		Offset(int(offset)).
+		Find(&es)
 	err = tx.Error
 	return
 }
