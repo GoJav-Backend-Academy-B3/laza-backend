fe8ae7cf10d406110ae4e15fc129801c5b3ff1ba
ariwiraa <ariwiraatmaja@gmail.com>
2023-08-09 14:35:00 +0700

    fix: change validation address

--- domain/requests/address_request.go
+++ domain/requests/address_request.go
@@ -1,9 +1,9 @@
 package requests
 
 type AddressRequest struct {
-	Country      string `validate:"required,alpha" json:"country,omitempty"`
-	City         string `validate:"required,alpha" json:"city,omitempty"`
-	ReceiverName string `validate:"required,alpha" json:"receiver_name,omitempty"`
+	Country      string `validate:"required" json:"country,omitempty"`
+	City         string `validate:"required" json:"city,omitempty"`
+	ReceiverName string `validate:"required" json:"receiver_name,omitempty"`
 	PhoneNumber  string `validate:"required,numeric,max=20" json:"phone_number,omitempty"`
 	IsPrimary    bool   `validate:"required" json:"is_primary,omitempty"`
 }
