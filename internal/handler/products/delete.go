fb889c5d805455a9b34123844f8855d4bd3f7bb8
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-09 15:17:28 +0700

    add swagger documentations

--- internal/handler/products/delete.go
+++ internal/handler/products/delete.go
@@ -8,6 +8,17 @@ import (
 	"github.com/phincon-backend/laza/helper"
 )
 
+// DeleteProduct godoc
+// @Summary Deletes product identified by the given id
+// @Description Deletes product corresponding to the input Id
+// @Tags product
+// @Produce json
+// @Param id path int true "ID of the product"
+// @Security JWT
+// @Success 200 {object} helper.Response{code=string,isError=bool,status=string,data=map[string]string}
+// @Failure 400 {object} helper.Response{code=int,description=string,isError=bool,description=map[string]string}
+// @Failure 500 {object} helper.Response{code=int,description=string,isError=bool,description=map[string]string}
+// @Router /products/{id} [delete]
 func (h *productHandler) delete(c *gin.Context) {
 
 	id_p := c.Param("id")
