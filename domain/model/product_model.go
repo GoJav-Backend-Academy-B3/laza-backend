a14ca8b7c28e32cde54d18bf02fe4d596c3fda68
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-10 15:26:19 +0700

    add DeletedAt type for soft delete

--- domain/model/product_model.go
+++ domain/model/product_model.go
@@ -1,23 +1,28 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
-	Id            uint64     `json:"id,omitempty" gorm:"primarykey"`
-	Name          string     `json:"name,omitempty"`
-	Description   string     `json:"description,omitempty"`
-	ImageUrl      string     `json:"image_url,omitempty"`
-	Price         float64    `json:"price,omitempty"`
-	CategoryId    uint64     `json:"category_id,omitempty"`
-	BrandId       uint64     `json:"brand_id,omitempty"`
-	CreatedAt     time.Time  `json:"created_at"`
-	UpdatedAt     time.Time  `json:"updated_at"`
-	Sizes         []Size     `json:"size" gorm:"many2many:size_product"`
-	Reviews       []Review   `json:"reviews" gorm:"foreignkey:Id"`
-	Categories    []Category `json:"categories" gorm:"many2many:category_product"` //many 2 many
-	CartUsers     []User     `json:"cart_users" gorm:"many2many:cart"`
-	WishlistUsers []User     `json:"wishlisted_users" gorm:"many2many:wishlist"`
-	Orders        []Order    `json:"orders" gorm:"many2many:product_order"`
+	Id            uint64         `json:"id,omitempty" gorm:"primarykey"`
+	Name          string         `json:"name,omitempty"`
+	Description   string         `json:"description,omitempty"`
+	ImageUrl      string         `json:"image_url,omitempty"`
+	Price         float64        `json:"price,omitempty"`
+	CategoryId    uint64         `json:"category_id,omitempty"`
+	BrandId       uint64         `json:"brand_id,omitempty"`
+	CreatedAt     time.Time      `json:"created_at"`
+	UpdatedAt     time.Time      `json:"updated_at"`
+	DeletedAt     gorm.DeletedAt `json:"deleted_at"`
+	Sizes         []Size         `json:"size" gorm:"many2many:size_product"`
+	Reviews       []Review       `json:"reviews" gorm:"foreignkey:Id"`
+	Categories    []Category     `json:"categories" gorm:"many2many:category_product"` //many 2 many
+	CartUsers     []User         `json:"cart_users" gorm:"many2many:cart"`
+	WishlistUsers []User         `json:"wishlisted_users" gorm:"many2many:wishlist"`
+	Orders        []Order        `json:"orders" gorm:"many2many:product_order"`
 }
 
 func (Product) TableName() string {
