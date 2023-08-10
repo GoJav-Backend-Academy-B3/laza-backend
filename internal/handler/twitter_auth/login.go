1c4893d33883318c891e394aed53aeaffd631c98
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-08-10 19:35:37 +0700

    edit twitter handler

--- internal/handler/twitter_auth/login.go
+++ internal/handler/twitter_auth/login.go
@@ -3,7 +3,6 @@ package twitterauth
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/markbates/goth/gothic"
-	"github.com/phincon-backend/laza/domain/response"
 	"github.com/phincon-backend/laza/helper"
 )
 
@@ -25,10 +24,6 @@ func (h *twitterAuthHandler) loginTwitter(c *gin.Context) {
 
 	gothic.Store = helper.GetStore()
 
-	if gothUser, err := gothic.CompleteUserAuth(c.Writer, c.Request); err == nil {
-		rb := response.FillFromTwitter(gothUser.Email, gothUser.Name, gothUser.NickName, gothUser.RawData["profile_image_url_https"].(string))
-		h.useCaseTwitter.Execute(rb).Send(c)
-	} else {
-		gothic.BeginAuthHandler(c.Writer, c.Request)
-	}
+	gothic.BeginAuthHandler(c.Writer, c.Request)
+
 }
