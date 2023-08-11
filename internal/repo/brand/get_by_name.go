c085eb9ce050c6a99edd1b9625ce6d14796ec359
ariwiraa <ariwiraatmaja@gmail.com>
2023-08-11 15:29:31 +0700

    fix: get by name brand

--- internal/repo/brand/get_by_name.go
+++ internal/repo/brand/get_by_name.go
@@ -3,7 +3,7 @@ package brand
 import "github.com/phincon-backend/laza/domain/model"
 
 func (r *BrandRepo) GetByName(brand string) (m model.Brand, err error) {
-	db := r.db.Where("brand = ?", brand).First(&m)
+	db := r.db.Where("name = ?", brand).First(&m)
 	err = db.Error
 	// TODO: Should return error here
 	return
