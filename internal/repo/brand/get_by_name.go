d5e5bc21620ba76348c4f2d46453303abca437ec
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-11 16:48:32 +0700

    implement 3 repositories method

--- internal/repo/brand/get_by_name.go
+++ internal/repo/brand/get_by_name.go
@@ -1,9 +1,20 @@
 package brand
 
-import "github.com/phincon-backend/laza/domain/model"
+import (
+	"context"
+
+	"github.com/phincon-backend/laza/domain/model"
+)
 
 func (r *BrandRepo) GetByName(brand string) (m model.Brand, err error) {
 	db := r.db.Where("brand = ?", brand).First(&m)
+	err = db.Error
+	// TODO: Should return error here
+	return
+}
+
+func (r *BrandRepo) GetByNameWithContext(ctx context.Context, brand string) (m model.Brand, err error) {
+	db := r.db.WithContext(ctx).Where("name = ?", brand).First(&m)
 	err = db.Error
 	// TODO: Should return error here
 	return
