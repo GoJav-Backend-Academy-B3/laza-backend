505f7935510328ef8ba70c7360180cfd62caef2e
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-11 16:49:37 +0700

    apply goroutines to perform 3 checks on repositories

--- internal/usecase/product/create_product_impl.go
+++ internal/usecase/product/create_product_impl.go
@@ -1,7 +1,9 @@
 package product
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
 	"github.com/phincon-backend/laza/domain/model"
 	"github.com/phincon-backend/laza/domain/repositories"
@@ -21,13 +23,13 @@ type CreateProductUsecaseImpl struct {
 	insertProductAction repositories.InsertAction[model.Product]
 
 	// get brand name
-	getBrandName brand.GetByNameAction
+	getBrandName brand.GetByNameActionWithContext
 
 	// Get size by name
-	getSizeAction size.GetByNameAction
+	getSizeAction size.GetByNameActionWithContext
 
 	// Get category by name
-	getCategoryAction category.GetByNameAction
+	getCategoryAction category.GetByNameActionWithContext
 }
 
 // Execute implements product.CreateProductUsecase.
@@ -35,23 +37,47 @@ func (u *CreateProductUsecaseImpl) Execute(request requests.ProductRequest) (pro
 
 	// Check if brand name exists
 	// return error if false
-	brand, err := u.getBrandName.GetByName(request.Brand)
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return product, errors.New("NotFound: Size not found")
-	}
+	errorChan := make(chan error, 3)
 
-	sizeModels := make([]model.Size, 0)
+	var brand model.Brand
+	var sizes []model.Size
+	var category model.Category
+
+	var taskCount int = 0
+	checkingContext, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	brandContext, _ := context.WithCancel(checkingContext)    // Cancel should be done on parent
+	sizeContext, _ := context.WithCancel(checkingContext)     // Cancel should be done on parent
+	categoryContext, _ := context.WithCancel(checkingContext) // Cancel should be done on parent
+
+	// Check existing name on repo
+	taskCount++
+	go u.getBrandNameRepo(brandContext, request.Brand, &brand, errorChan, &taskCount)
+
+	// Check existing size on repo
 	for _, v := range request.Sizes {
-		sz, err := u.getSizeAction.GetByName(v)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return product, errors.New("NotFound: Size not found")
-		}
-		sizeModels = append(sizeModels, sz)
+		insideSizeContext, _ := context.WithCancel(sizeContext) // Cancel should be done on parent's parent
+		taskCount++
+		go u.getSizeRepo(insideSizeContext, v, &sizes, errorChan, &taskCount)
 	}
 
-	category, err := u.getCategoryAction.GetByName(request.Category)
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return product, errors.New("NotFound: Category not found")
+	// Check existing category on repo
+	taskCount++
+	go u.getCategoryRepo(categoryContext, request.Category, &category, errorChan, &taskCount)
+
+	for e := range errorChan {
+		if e != nil {
+			// EMERGENCY! CANCEL ALL ROUTINES
+			cancel()
+			fmt.Println("chan error: ", e.Error())
+			err = e
+			return
+		}
+		taskCount--
+		if taskCount == 0 {
+			break
+		}
 	}
 
 	file, err := request.Image.Open()
@@ -71,7 +97,7 @@ func (u *CreateProductUsecaseImpl) Execute(request requests.ProductRequest) (pro
 		Price:       request.Price,
 		CategoryId:  category.Id,
 		BrandId:     brand.Id, // didapat dari search by brand, ambil salah satu brand id
-		Sizes:       sizeModels,
+		Sizes:       sizes,
 	})
 	if err != nil {
 		return
@@ -80,11 +106,52 @@ func (u *CreateProductUsecaseImpl) Execute(request requests.ProductRequest) (pro
 	return
 }
 
+func (u *CreateProductUsecaseImpl) getBrandNameRepo(ctx context.Context, brandName string, brand *model.Brand, errChan chan<- error, taskCount *int) {
+	defer func() { *taskCount-- }()
+
+	var err error
+	*brand, err = u.getBrandName.GetByNameWithContext(ctx, brandName)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		errChan <- errors.New("NotFound: Brand not found")
+		return
+	}
+	errChan <- err
+}
+
+func (u *CreateProductUsecaseImpl) getSizeRepo(ctx context.Context, sizeName string, sizes *[]model.Size, errChan chan<- error, taskCount *int) {
+	defer func() { *taskCount-- }()
+
+	sz, err := u.getSizeAction.GetByNameWithContext(ctx, sizeName)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		errChan <- errors.New("NotFound: Size not found")
+		return
+	} else if err != nil {
+		errChan <- err
+		return
+	}
+	*sizes = append(*sizes, sz)
+	errChan <- nil
+}
+
+func (u *CreateProductUsecaseImpl) getCategoryRepo(ctx context.Context, categoryName string, category *model.Category, errChan chan<- error, taskCount *int) {
+	defer func() { *taskCount-- }()
+
+	var err error
+	*category, err = u.getCategoryAction.GetByNameWithContext(ctx, categoryName)
+	// BUG: This sometimes not emitting gorm.ErrRecordNotFound when inputting
+	//      nonexistent errors.
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		errChan <- errors.New("NotFound: Category not found")
+		return
+	}
+	errChan <- err
+}
+
 func NewCreateProductUsecaseImpl(
 	insertProductAction repositories.InsertAction[model.Product],
-	searchByBrandAction brand.GetByNameAction,
-	getSizeAction size.GetByNameAction,
-	getCategoryAction category.GetByNameAction) usecase.CreateProductUsecase {
+	searchByBrandAction brand.GetByNameActionWithContext,
+	getSizeAction size.GetByNameActionWithContext,
+	getCategoryAction category.GetByNameActionWithContext) usecase.CreateProductUsecase {
 	return &CreateProductUsecaseImpl{
 		insertProductAction: insertProductAction,
 		getBrandName:        searchByBrandAction,
