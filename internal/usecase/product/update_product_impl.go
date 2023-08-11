94801e69c4da8b63f4cfc46fdacdf4a77f1e4fb8
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-11 13:47:20 +0700

    import brand domain repo

--- internal/usecase/product/update_product_impl.go
+++ internal/usecase/product/update_product_impl.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/phincon-backend/laza/domain/model"
 	"github.com/phincon-backend/laza/domain/repositories"
+	"github.com/phincon-backend/laza/domain/repositories/brand"
 	"github.com/phincon-backend/laza/domain/repositories/category"
 	"github.com/phincon-backend/laza/domain/repositories/product"
 	"github.com/phincon-backend/laza/domain/repositories/size"
