32c94a682e1fd6ccbfe94c6ca89a4eb418044afc
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-08-14 15:23:52 +0700

    edit delete cart repo

--- internal/repo/cart/delete_all_user_cart.go
+++ internal/repo/cart/delete_all_user_cart.go
@@ -2,10 +2,9 @@ package cart
 
 import "github.com/phincon-backend/laza/domain/model"
 
-func (r *CartRepo) Delete(userId any) (value string, err error) {
+func (r *CartRepo) Delete(userId any) (err error) {
 	user_id := userId.(uint64)
 	tx := r.db.Where("user_id = ?", user_id).Delete(&model.Cart{})
-	value = "successfully delete user cart"
 	err = tx.Error
 	return
 }
